85a4814eed46831de21177cd3bb7606dc6a33077
Joseph Penton <josephpenton2@gmail.com>
2023-10-11 00:05:26 -0500

    finished

--- Homework08/gpenton.hw8.js
+++ Homework08/gpenton.hw8.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const userSubmit = document.getElementById('Submit');
     const userGrade = document.getElementById('input');
+    const userResult = document.getElementById('result');
 
     userSubmit.addEventListener('click', function() {
-        if (parseFloat(userGrade.value) >= 90) {
-            console.log("Yippie");
+        switch (true) {
+            case userGrade.value >= 90:
+                console.log("Candy");
+                userResult.innerHTML = "You Win Candy!"
+                break;
+            case userGrade.value >= 80:
+                console.log("Apple");
+                userResult.innerHTML = "You Win A Apple"
+                break;
+            case userGrade.value >= 70:
+                console.log("Mint");
+                userResult.innerHTML = "You Win A Mint"
+                break;
+            case userGrade.value >= 60:
+                console.log("Pencil");
+                userResult.innerHTML = "You Win A Pencil"
+                break;
+            default:
+                console.log("Awkward Talk With Parents");
+                userResult.innerHTML = "You win an Awkward Talk With Your Parents :("
         }
+
     });
 });
