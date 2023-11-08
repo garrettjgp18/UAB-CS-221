d35ef239db31f3671454b0ff21ffc24e01be7296
Joseph Penton <josephpenton2@gmail.com>
2023-11-08 09:31:03 -0600

    Created and modified

---
+++ Lab10/gpenton.lab10.js
@@ -0,0 +1,74 @@
+// Makes sure Javascript loads last to avoid errors
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Initialize multiple variables 
+    // Sets up EventListeners later on
+    let rockChoice = document.getElementById('rock');
+    let paperChoice = document.getElementById('paper');
+    let scissorChoice = document.getElementById('scissors');
+
+    let gameResult = document.getElementById('result');
+
+    // Initialize variable to keep track of players choice
+    let playerChoice;
+
+    // Uses variable from above to give the div with the ID "rock" an EventListener that will set the playerChoice variable
+    // to rock, as well as call the checkChoice function, passing in the playerChoice value. 
+    rockChoice.addEventListener('click', function() {
+        playerChoice = "rock";
+        console.log("Rock clicked");
+        checkChoice(playerChoice);
+    });
+
+    // Similar to above
+    paperChoice.addEventListener('click', function() {
+        playerChoice = "paper";
+        console.log("Paper clicked")
+        checkChoice(playerChoice);
+    });
+
+    // Similar to above
+    scissorChoice.addEventListener('click', function() {
+        playerChoice = "scissors";
+        console.log("Scissors clicked");
+        checkChoice(playerChoice);
+    });
+    
+    // Initialize array that will allow computer to index with the Math.random function, choosing a random choice.
+    let choiceArray = ["rock", "paper", "scissors" ];
+
+    /* Purpose: Generate a random index, and use it to index a random element from the choiceArray. Compare it to playerChoice.
+    *  Input: playerChoice (string)
+    *  Output: Console output of win or loss.
+    */ 
+    function checkChoice(playerChoice) {
+        let computerChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+        
+        // if (playerChoice == choiceArray[computerChoice]) {
+        //     console.log("You Win!\n");
+        //     console.log(" ");
+        //     gameResult.innerHTML = "You Win!";
+        // } else {
+        //     console.log("You Loose!\n");
+        //     console.log(" ");
+        //     gameResult.innerHTML = "You Loose...";
+        // }
+
+        if (playerChoice == computerChoice) {
+            console.log("Computer chose: " + computerChoice);
+            console.log("It's a tie!");
+            console.log(" ")
+            gameResult.innerHTML = "Computer chose " + computerChoice + ", It's a tie!\n"
+        } else if ((playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "paper" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "rock")){
+            console.log("Computer chose: " + computerChoice);
+            console.log("You loose...");
+            console.log(" ")
+            gameResult.innerHTML = "Computer chose " + computerChoice +", You lose...\n"
+        } else {
+            console.log("Computer chose: " + computerChoice);
+            console.log("You Win!");
+            console.log(" ")
+            gameResult.innerHTML = "Computer chose " + computerChoice + ", You win!\n"
+        }
+    }
+});
